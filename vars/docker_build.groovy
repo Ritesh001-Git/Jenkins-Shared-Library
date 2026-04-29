321a2b0f2fd1d65826b60d46b44512bd6815af81
Ritesh Swain <152496676+Ritesh001-Git@users.noreply.github.com>
2026-04-30 00:00:53 +0530

    Add Docker build function in docker_build.groovy

---
+++ vars/docker_build.groovy
@@ -0,0 +1,12 @@
+def call(Map config = [:]){
+  def imageName = config.imageName ?: error("Image name is required")
+  def imageTag = config.imagrTag ?: 'latest'
+  def dockerfile = config.dockerfile ?: 'dockerfile'
+  def context = config.context ?: '.'
+
+  echo "Building the Docker Image ${imageName}:${imageTag} using ${Dockerfile}"
+  
+  sh """
+  docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
+  """
+}
